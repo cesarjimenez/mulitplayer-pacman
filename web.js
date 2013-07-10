fd1109e28cfcbc5fe8ab55927cece6255b1fe2bc
César <cesiro@MacBook-Air-de-Cesar.local>
2013-07-10 19:20:32 +0200

    Added a POST method that can change the greeting message.

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.bodyParser()); // Needed to parse POST requests
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
+var message = "Hello World!"
+
+app.get("/message", function(request, response) {
+    response.send(message + "\n");
+});
+
+app.post("/newmessage", function(request, response) {
+    console.log("Received POST request!");
+    message = request.body.newmessage;
+    response.send("new message is " + message + "\n");
 });
 
 var port = process.env.PORT || 5000;
